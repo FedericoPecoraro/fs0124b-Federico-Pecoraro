d654ef434f1acd82323aa65b6104433775763aa1
FedericoPecoraro <federico.pecoraro8@gmail.com>
2024-05-16 16:37:04 +0200

    g4

---
+++ m4/s3/g1/Clienti.sql
@@ -0,0 +1,12 @@
+INSERT INTO "Exercise"."Clienti"(
+	"NumeroCliente", "Nome", "Cognome", "AnnoDiNascita", "RegioneDiResidenza")
+	VALUES (1, 'Freddie', 'Mercury', 1946, 'Londra'),
+(2, 'Mick', 'Jagger', 1943, 'Londra'),
+(3, 'David', 'Bowie', 1947, 'Londra'),
+(4, 'Robert', 'Plant', 1948, 'Inghilterra'),
+(5, 'Jimi', 'Hendrix', 1942, 'Stati Uniti'),
+(6, 'Elvis', 'Presley', 1935, 'Stati Uniti'),
+(7, 'Kurt', 'Cobain', 1967, 'Stati Uniti'),
+(8, 'John', 'Lennon', 1940, 'Inghilterra'),
+(9, 'Roger', 'Waters', 1943, 'Inghilterra'),
+(10, 'Eric', 'Clapton', 1945, 'Inghilterra');
---
+++ m4/s3/g1/Prodotti.sql
@@ -0,0 +1,12 @@
+INSERT INTO "Exercise"."Prodotti"(
+	"IdProdotto", "Descrizione", "InProduzione", "InCommercio", "DataAttivazione", "DataDisattivazione")
+	VALUES (1, 'Chitarra elettrica Fender Stratocaster', True, True, '2022-01-01', '2022-12-31'),
+(2, 'Batteria Yamaha Stage Custom', True, True, '2019-06-01', '2023-12-31'),
+(3, 'Pianoforte a coda Steinway & Sons', True, True, '2018-03-15', '2021-12-31'),
+(4, 'Basso Fender Precision', True, True, '2021-02-01', '2025-12-31'),
+(5, 'Sax contralto Selmer Mark VI', False, False, '1900-01-01', '2023-07-01'),
+(6, 'Violino Stradivari', False, True, '1900-01-01', '1995-12-31'),
+(7, 'Tamburo africano djembe', True, True, '2022-09-01', '2026-12-31'),
+(8, 'Tromba Bach Stradivarius', True, True, '2020-11-01', '2023-12-31'),
+(9, 'Arpa celtica Lever', True, True, '2023-04-01', '2026-12-31'),
+(10, 'Synth Roland Juno-106', False, True, '1900-01-01', '1997-12-31');
