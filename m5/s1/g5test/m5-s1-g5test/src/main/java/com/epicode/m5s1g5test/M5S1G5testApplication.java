026c0fd8e81fb436fefaa7039790b27b80c8ff38
FedericoPecoraro <federico.pecoraro8@gmail.com>
2024-05-17 10:42:22 +0200

    start

---
+++ m5/s1/g5test/m5-s1-g5test/src/main/java/com/epicode/m5s1g5test/M5S1G5testApplication.java
@@ -0,0 +1,13 @@
+package com.epicode.m5s1g5test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M5S1G5testApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M5S1G5testApplication.class, args);
+	}
+
+}
---
+++ m5/s1/g5test/m5-s1-g5test/src/test/java/com/epicode/m5s1g5test/M5S1G5testApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epicode.m5s1g5test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M5S1G5testApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
